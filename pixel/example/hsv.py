f96d7c0a69369398f1397b721e72637385f33410
Aditeya <22963960+Aditeya@users.noreply.github.com>
2019-03-19 18:18:13 +0400

    fixed hsv.py in example folder

--- pixel/example/hsv.py
+++ pixel/example/hsv.py
@@ -9,12 +9,14 @@ v = 1
 
 
 for hue in range(360):
-    rgb = colorsys.hsv_to_rgb(hue/360.0, s, v)
-    leds=[rgb]*360
-    client.putPixels(leds)
-    print hue, 
-    print rgb
-    time.sleep(1)
+	rgb = colorsys.hsv_to_rgb(hue/360.0, s, v)
+	leds=[rgb]*360
+	client.put_pixels(leds)
+	print(hue)
+	print(rgb)
+	s+=1
+	v+=1
+	time.sleep(1/30.0)
     
     
 
